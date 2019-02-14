60fa32aa651f2af24aa792441fc89fbfe06f948f
manojmpg114 <manojmpg114@aol.com>
2019-02-14 16:01:35 -0500

    Add files via upload

--- senior_project_pickerpal/lib/main.dart
+++ senior_project_pickerpal/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 void main() => runApp(MyApp());
 int item = 0;
 bool clicked = false;
