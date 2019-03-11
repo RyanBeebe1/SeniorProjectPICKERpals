6025b1eaca6f71f3b78aad059293debcef331039
RyanBeebe1 <33845040+RyanBeebe1@users.noreply.github.com>
2019-03-11 14:44:58 -0400

    Added camera functionality
    Allow user to take a picture or upload photo from camera roll

--- senior_project_pickerpal/lib/upload_item.dart
+++ senior_project_pickerpal/lib/upload_item.dart
@@ -1,7 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class upload_item extends StatefulWidget {
-
   upload_item({Key key, this.tag}) : super(key: key);
   final String tag;
   @override
@@ -9,7 +10,39 @@ class upload_item extends StatefulWidget {
 }
 
 class _uploadItemState extends State<upload_item> {
+  File _image;
   String dropdownValue = null;
+
+  Future getImage() async {
+    var image;
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: Text("Choose image"),
+          children: <Widget>[
+            SimpleDialogOption(onPressed: () async {
+              image = await ImagePicker.pickImage(source: ImageSource.camera);
+              setState(() {
+                _image = image;
+                Navigator.of(context).pop();
+              });
+            },
+              child: Text("Take Photo"),),
+            SimpleDialogOption(onPressed: () async {
+              image = await ImagePicker.pickImage(source: ImageSource.gallery);
+              setState(() {
+                _image = image;
+                Navigator.of(context).pop();
+              });
+            },
+              child: Text("Choose Photo"),)
+          ],
+        );
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -17,6 +50,9 @@ class _uploadItemState extends State<upload_item> {
       appBar: AppBar(
         leading: FlatButton(onPressed: () {Navigator.of(context).pop();}, child: Icon(Icons.close)),
         title: Text("Upload Item"),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.add_a_photo), onPressed: getImage)
+        ],
       ),
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
@@ -27,12 +63,12 @@ class _uploadItemState extends State<upload_item> {
           children: <Widget>[
             Padding(padding: EdgeInsets.all(20.0)),
             Container(
-              color: Colors.grey,
+              color: _image == null?Colors.grey:Colors.transparent,
               height: 100,
               width: 100,
               child: Center(
-                child: Icon(Icons.add),
-              ),
+                child: _image == null?Text("No Image Selected", textAlign: TextAlign.center,):Image.file(_image),
+              )
             ),
             Padding(padding: EdgeInsets.all(20.0)),
               Flexible(
