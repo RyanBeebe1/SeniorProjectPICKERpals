9c6d55334dc9110cde1f7d16b1aebd067b021f24
Ryan Beebe <beeber8@robirobi4474.fios-router.home>
2019-03-28 21:55:49 -0400

    Purging repository to clean up with only files we need

--- senior_project_backend/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:senior_project_backend/ui/listview_note.dart';
-
-void main() => runApp(
-  MaterialApp(
-    title: 'Returning Data',
-    home: ListViewNote(),
-  ),
-);
