6497c50b7897fbba783880a3ba9819b35b9e8f08
Joe <jomac718@yahoo.com>
2019-03-30 11:06:23 -0400

    add session data file

---
+++ senior_project_pickerpal/lib/session.dart
@@ -0,0 +1,10 @@
+import 'package:senior_project_pickerpal/pickup_entry.dart';
+
+class SessionVariables {
+
+
+  static String loggedInEmail;
+  static bool loggedIn = false;
+  static User user;
+  
+}
