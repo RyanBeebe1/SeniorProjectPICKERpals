f17c9a064973b4c915c8e6916268793a7544c288
joeywhitt <joeywhitt@gmail.com>
2019-04-20 15:36:49 -0400

    Changed formatting for chats so the title is always the opposing user

--- senior_project_pickerpal/lib/chat.dart
+++ senior_project_pickerpal/lib/chat.dart
@@ -6,7 +6,8 @@ import 'package:seniorprojectnuked/session.dart';
 import 'package:intl/intl.dart';
 
 class Chat extends StatefulWidget {
-  Chat({Key key, this.receiverId,this.senderId,this.myChats}) : super(key: key);
+  Chat({Key key, this.receiverId, this.senderId, this.myChats})
+      : super(key: key);
   final int receiverId;
   final int senderId;
   final bool myChats;
@@ -24,39 +25,45 @@ class ChatWindow extends State<Chat> with TickerProviderStateMixin {
     String formattedDate = DateFormat('yyyy-MM-dd HH:mm:ss').format(now);
     return formattedDate;
   }
-  
+
   Future<void> _getMessages() async {
-    List<UserChat> chats = await BackendService.fetchChats("http://ec2-3-88-8-44.compute-1.amazonaws.com:5000/getchats/"+SessionVariables.user.userId.toString());
+    List<UserChat> chats = await BackendService.fetchChats(
+        "http://ec2-3-88-8-44.compute-1.amazonaws.com:5000/getchats/" +
+            SessionVariables.user.userId.toString());
     List<Message> messages;
     if (chats.length > 0) {
-    for (UserChat c in chats) {
-        if ((c.sender.userId == widget.senderId && c.recipient.userId == widget.receiverId)||
-        (c.sender.userId == widget.receiverId && c.recipient.userId == widget.senderId)){
-          messages = await BackendService.fetchMessages("http://ec2-3-88-8-44.compute-1.amazonaws.com:5000/getmessages/"+c.chat_id.toString());
+      for (UserChat c in chats) {
+        if ((c.sender.userId == widget.senderId &&
+                c.recipient.userId == widget.receiverId) ||
+            (c.sender.userId == widget.receiverId &&
+                c.recipient.userId == widget.senderId)) {
+          messages = await BackendService.fetchMessages(
+              "http://ec2-3-88-8-44.compute-1.amazonaws.com:5000/getmessages/" +
+                  c.chat_id.toString());
         }
-    }
+      }
     }
     if (messages != null) {
-    for (Message m in messages) {
-      String name = m.user.displayName;
-      setState(() {
-         _messages.add(new Msg(txt:m.body,animationController: new AnimationController(
-          vsync: this,
-        duration: new Duration(milliseconds: 800)
-      ),name: name,));
-      });
-     
-    }
+      for (Message m in messages) {
+        String name = m.user.displayName;
+        setState(() {
+          _messages.add(new Msg(
+            txt: m.body,
+            animationController: new AnimationController(
+                vsync: this, duration: new Duration(milliseconds: 800)),
+            name: name,
+          ));
+        });
+      }
     }
   }
+
   @override
   void initState() {
-      super.initState();
-      _getMessages().then((hi) {
-      });
-   
-
+    super.initState();
+    _getMessages().then((hi) {});
   }
+
   @override
   Widget build(BuildContext ctx) {
     return new Scaffold(
@@ -66,11 +73,11 @@ class ChatWindow extends State<Chat> with TickerProviderStateMixin {
       body: new Column(children: <Widget>[
         new Flexible(
             child: new ListView.builder(
-              itemBuilder: (ctx, int index) => _messages[index],
-              itemCount: _messages.length,
-              reverse: true,
-              padding: new EdgeInsets.all(6.0),
-            )),
+          itemBuilder: (ctx, int index) => _messages[index],
+          itemCount: _messages.length,
+          reverse: true,
+          padding: new EdgeInsets.all(6.0),
+        )),
         new Divider(height: 1.0),
         new Container(
           child: _buildComposer(),
@@ -80,33 +87,32 @@ class ChatWindow extends State<Chat> with TickerProviderStateMixin {
     );
   }
 
-
   Widget _buildComposer() {
     return new IconTheme(
-        data: new IconThemeData(color: Theme.of(context).accentColor),
-        child: new Container(
-          margin: const EdgeInsets.symmetric(horizontal: 9.0),
-          child: new Row(
-            children: <Widget>[
-              new Flexible(
-                  child: new TextField(
-                    controller: _textController,
-                    onChanged: (String txt) {
-                      setState(() {
-                        _isWriting = txt.length > 0;
-                      });
-                    },
-                    onSubmitted: _submitMsg,
-                    decoration:
-                      new InputDecoration.collapsed(hintText: "Enter some text to send a message"),
-                  ),
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 9.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onChanged: (String txt) {
+                  setState(() {
+                    _isWriting = txt.length > 0;
+                  });
+                },
+                onSubmitted: _submitMsg,
+                decoration: new InputDecoration.collapsed(
+                    hintText: "Enter some text to send a message"),
               ),
-              new Container(
-                margin: new EdgeInsets.symmetric(horizontal: 3.0),
-              ),
-            ],
-          ),
+            ),
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 3.0),
+            ),
+          ],
         ),
+      ),
     );
   }
 
@@ -116,21 +122,26 @@ class ChatWindow extends State<Chat> with TickerProviderStateMixin {
       _isWriting = false;
     });
     Msg msg = new Msg(
-      txt: txt,
-      animationController: new AnimationController(
-          vsync: this,
-        duration: new Duration(milliseconds: 800)
-      ),name: SessionVariables.user.displayName
-    );
+        txt: txt,
+        animationController: new AnimationController(
+            vsync: this, duration: new Duration(milliseconds: 800)),
+        name: SessionVariables.user.displayName);
     setState(() {
       _messages.insert(0, msg);
       print(_messages.length);
     });
     if (!widget.myChats) {
-    BackendService.addMessage(new Message(body:txt, date:_getDateTime(), user: SessionVariables.user), SessionVariables.user.userId, widget.receiverId);
-    }
-    else {
-    BackendService.addMessage(new Message(body:txt, date:_getDateTime(), user: SessionVariables.user), widget.senderId, widget.receiverId);
+      BackendService.addMessage(
+          new Message(
+              body: txt, date: _getDateTime(), user: SessionVariables.user),
+          SessionVariables.user.userId,
+          widget.receiverId);
+    } else {
+      BackendService.addMessage(
+          new Message(
+              body: txt, date: _getDateTime(), user: SessionVariables.user),
+          widget.senderId,
+          widget.receiverId);
     }
     msg.animationController.forward();
   }
@@ -142,27 +153,27 @@ class ChatWindow extends State<Chat> with TickerProviderStateMixin {
     }
     super.dispose();
   }
-
 }
 
 class Msg extends StatelessWidget {
-  Msg({this.txt, this.animationController,this.name});
+  Msg({this.txt, this.animationController, this.name});
   final String txt;
   final AnimationController animationController;
   final String name;
   @override
   Widget build(BuildContext ctx) {
     return ListTile(
-      leading: new CircleAvatar(child: new Text(name.substring(0,1).toUpperCase())),
-      title:  Text(name),
-      subtitle:  Text(txt),
-      );
+      leading:
+          new CircleAvatar(child: new Text(name.substring(0, 1).toUpperCase())),
+      title: Text(name),
+      subtitle: Text(txt),
+    );
   }
 }
 
 class MyChats extends StatefulWidget {
   MyChats();
- 
+
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
@@ -170,21 +181,37 @@ class MyChats extends StatefulWidget {
   }
 }
 
- class MyChatsState extends State<MyChats> {
+class MyChatsState extends State<MyChats> {
   List<ChatTile> _chats = <ChatTile>[];
 
   Future<void> _buildTiles() async {
-      List<UserChat> chat = await BackendService.fetchChats("http://ec2-3-88-8-44.compute-1.amazonaws.com:5000/getchats/"+SessionVariables.user.userId.toString());
-      if (chat.length > 0) {
-        for (UserChat c in chat) {
-          Message m = await BackendService.fetchLastMessage("http://ec2-3-88-8-44.compute-1.amazonaws.com:5000/lastmessage/"+c.chat_id.toString());
-          setState(() {
-            _chats.add(new ChatTile(name: m.user.displayName,txt: m.body,senderId: c.sender.userId,receiverId: c.recipient.userId,));
-          });
-               
+    List<UserChat> chat = await BackendService.fetchChats(
+        "http://ec2-3-88-8-44.compute-1.amazonaws.com:5000/getchats/" +
+            SessionVariables.user.userId.toString());
+    if (chat.length > 0) {
+      for (UserChat c in chat) {
         
+        Message m = await BackendService.fetchLastMessage(
+            "http://ec2-3-88-8-44.compute-1.amazonaws.com:5000/lastmessage/" +
+                c.chat_id.toString());
+        
+        String otherUser;
+        
+        if (SessionVariables.user.userId == c.sender.userId) {
+          otherUser = c.recipient.displayName;
+        } else {
+          otherUser = c.sender.displayName;
         }
+        setState(() {
+          _chats.add(new ChatTile(
+            name: otherUser,
+            txt: m.body,
+            senderId: c.sender.userId,
+            receiverId: c.recipient.userId,
+          ));
+        });
       }
+    }
   }
 
   @override
@@ -192,6 +219,7 @@ class MyChats extends StatefulWidget {
     super.initState();
     _buildTiles();
   }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -202,19 +230,18 @@ class MyChats extends StatefulWidget {
       body: new Column(children: <Widget>[
         new Flexible(
             child: new ListView.builder(
-              itemBuilder: (ctx, int index) => _chats[index],
-              itemCount: _chats.length,
-              padding: new EdgeInsets.all(6.0),
-            )),
+          itemBuilder: (ctx, int index) => _chats[index],
+          itemCount: _chats.length,
+          padding: new EdgeInsets.all(6.0),
+        )),
         new Divider(height: 1.0),
       ]),
     );
   }
-    
-  }
+}
 
-  class ChatTile extends StatelessWidget {
-  ChatTile({this.txt,this.name,this.receiverId,this.senderId});
+class ChatTile extends StatelessWidget {
+  ChatTile({this.txt, this.name, this.receiverId, this.senderId});
   final String txt;
   final String name;
   final int senderId;
@@ -223,11 +250,19 @@ class MyChats extends StatefulWidget {
   Widget build(BuildContext ctx) {
     return ListTile(
       onTap: () {
-        Navigator.push(ctx, new MaterialPageRoute(builder: (context) => new Chat(senderId: senderId,receiverId: receiverId,myChats: true,)));
+        Navigator.push(
+            ctx,
+            new MaterialPageRoute(
+                builder: (context) => new Chat(
+                      senderId: senderId,
+                      receiverId: receiverId,
+                      myChats: true,
+                    )));
       },
-      leading: new CircleAvatar(child: new Text(name.substring(0,1).toUpperCase())),
-      title:  Text(name),
-      subtitle:  Text(txt),
-      );
+      leading:
+          new CircleAvatar(child: new Text(name.substring(0, 1).toUpperCase())),
+      title: Text(name),
+      subtitle: Text(txt),
+    );
   }
 }
